255bdf917cca5cdf5d7d732a60680549690b4598
Sabiha Afrin Siddiqua <sabihaafrin159@gmail.com>
2022-06-17 13:57:04 +0600

    Add files via upload

---
+++ migrations/2022_06_15_122625_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string("Name");
+            $table->int("Age");
+            $table->string("Address");
+            $table->string("Contact");
+            $table->unsignedbigInteger("user_id");
+            $table->int("Bed No.");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
